1825a816c518bed9e1487ef2b6081d02b3ab9f4f
M-Vallejo7 <giraldomiguel21@gmail.com>
2024-04-02 09:01:19 -0500

    Actualización

--- app/src/main/java/com/vallejo/entrega3/MainActivity.java
+++ app/src/main/java/com/vallejo/entrega3/MainActivity.java
@@ -35,21 +35,21 @@ public class MainActivity extends AppCompatActivity {
                 "Ronaldo Luís Nazario de Lima",
         };
         String[] biografias = {
-                "The G.O.A.T, El Comandante, El Bicho, CR7, La Máquina, Mr. Champions, El Batipibe. 5 veces ganador del Ballon d'Or. 5 veces campeón de UEFA Champions League. Campeón de Eurocopa 2016 y Campeón de UEFA Nations League en 2019. Máximo goleador de la historia del fútbol, máximo goleador de la champions, máximo goleador del Real Madrid. Puskás 2008.",
-                "Top #1 de la historia de Colombia. Mejor jugador del mundial Brasil 2014. Puskás 2014. El 10 del Real Madrid. Dos veces campeón de UEFA Champions League. Campeón de la Bundesliga.",
-                "La Tortuga. Donatello. Hat-Trick en una final del mundo vs Argentina. Actual mejor jugador del mundo. Campeón del mundo en 2018. Mejor jugador de la League One.",
-                "El último heredero del Jogo Bonito. El príncipe que no quiso ser Rey. El pájaro carpintero. Campeón de UEFA Champions League. Top#3 Mejores jugadores de la década. Ídolo de la selección de Brasil.",
-                "O´fenômeno. R9. El mejor delantero de la historia. El jugador mas joven en ganar el balón de oro. Dos veces campeón del mundo. Ídolo de la selección Brasileña.",
+                "The G.O.A.T, El Comandante, El Bicho, CR7, La Máquina, Mr. Champions, El Batipibe. 5 veces ganador del Ballon d'Or. 5 veces campeón de UEFA Champions League. Campeón de Eurocopa 2016 y Campeón de UEFA Nations League en 2019. Máximo goleador de la historia del fútbol, máximo goleador de la champions, máximo goleador del Real Madrid. Puskás 2008. Último ganador del Ballon d'Or jugando en Premier League.",
+                "Mejor jugador absoluto de la historia de la selección Colombia. Mejor jugador del mundial Brasil 2014. Puskás 2014. El 10 del Real Madrid. Dos veces campeón de UEFA Champions League y UEFA Europa League. Campeón de Bundesliga y La Liga Santander.",
+                "La Tortuga. Donatello. Hat-Trick en una final del mundo vs la Argentina de Lionel Messi. Actual mejor jugador del mundo. Campeón del mundo en 2018. Mejor jugador y máximo goleaor de la League One.",
+                "El último heredero del Jogo Bonito. El príncipe que no quiso ser Rey. El pájaro carpintero. Campeón de UEFA Champions League. Top#3 Mejores jugadores de la década. Ídolo de la selección Brasileña.",
+                "O´fenômeno. R9. El mejor delantero de la historia. El jugador mas joven en ganar el balón de oro. Dos veces campeón del mundo con la selección de Brasil. Ídolo de la selección Brasileña.",
         };
         String[] telefonos = {
-                "625 mill",
-                "51.9 mill",
-                "112 mill",
-                "220 mill",
-                "30 mill",
+                "625M",
+                "51.9M",
+                "112M",
+                "220M",
+                "30M",
         };
 
-        ListAdapter personas = new ListAdapter(MainActivity.this,nombres,telefonos,fotoPerfil,biografias);
+        ListAdapter personas = new ListAdapter(MainActivity.this,nombres,telefonos,fotoPerfil,biografias, visitCountMap);
         listaPersona = (ListView) findViewById(R.id.listaUsuario);
         listaPersona.setAdapter(personas);
 
